69a2fec12ed4c67d712ba89d1b5e1f3793b4f450
unknown <yogenial87@gmail.com>
2022-06-18 19:37:39 -0300

    prueba 1

---
+++ apuntes-git.js
@@ -0,0 +1,21 @@
+
+
+    
+const apuntes = `
+
+git init       : "iniciar un repositorio (proyecto)"
+git status -s  : checkear el documento
+git add .      : agregar a la caja
+git commit -m "etiqueta" : etiquetar caja
+
+`
+
+
+
+
+
+
+
+
+
+
