ad7bcdd20be3a62758a445395ac624154a208a67
unknown <yogenial87@gmail.com>
2022-06-18 19:43:42 -0300

    agregue hhh en el index

--- apuntes-git.js
+++ apuntes-git.js
@@ -7,6 +7,8 @@ git init       : "iniciar un repositorio (proyecto)"
 git status -s  : checkear el documento
 git add .      : agregar a la caja
 git commit -m "etiqueta" : etiquetar caja
+git log --oneline : mostrar etiquetas de cajas
+
 
 `
 
